51fc4292c3375d9f9c68cd26f644397876cb257f
Bo Vh <zhangbo9299@gmail.com>
2018-08-31 10:21:25 +0800

    增加功能
    计数功能、输入X退出、输入检测（输入值1~10之外，提示错误信息）

--- cards.c
+++ cards.c
@@ -8,28 +8,39 @@
 int main()
 {
     char card_name[3];
-    puts("输入牌名：");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]){
-        case 'J':
-        case 'Q':
-        case 'K':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-    }
-    /* 检查牌的点数是否在3到6之间 */
-    if (val >= 3 && val <= 6){
-        puts("计数增加");
-    }
-    /* 否则，检查牌是否是10，J，Q */
-    else if(val == 10){
-        puts("计数减少");
-    }
+    int count = 0;
+
+    do{
+        puts("输入牌名：");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0]){
+            case 'J':
+            case 'Q':
+            case 'K':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;//回到 while 处检查循环条件
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10)){
+                    puts("我无法理解这个值！");
+                    continue;//回到 while 处检查循环条件
+                }
+        }
+        /* 检查牌的点数是否在3到6之间 */
+        if (val >= 3 && val <= 6){
+            count++;
+        }
+        /* 否则，检查牌是否是10，J，Q */
+        else if(val == 10){
+            count--;
+        }
+        printf("当前的计数：%i\n", count);
+    }while(card_name[0] != 'X');
     return 0;
 }
