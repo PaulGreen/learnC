d8eb6d1cd7d998f089f1e36b787eb4c89f994739
Bo Vh <zhangbo9299@gmail.com>
2018-09-01 11:11:37 +0800

    增加经纬度范围检测、错误代码2

--- stdio_to_JSON.c
+++ stdio_to_JSON.c
@@ -14,6 +14,16 @@ int main(){
         else{
             started = 1;
         }
+        //检查经纬度是否在正确的范围内，超出范围返回错误代码：2
+        if ((latitude < -90.0) || (latitude > 90.0)){
+            printf("Invalid latitude: %f\n", latitude);
+            return 2;
+        }
+        if ((longitude < -180.0) || (longitude > 180.0)){
+            printf("Invalid longitude: %f\n", longitude);
+            return 2;
+        }
+
         printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
     }
     puts("\n");
