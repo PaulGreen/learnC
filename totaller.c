1b3e8aa7fd87984a4885551f1f6b73ddfadbd0bd
Bo Vh <zhangbo9299@gmail.com>
2018-09-05 08:26:26 +0800

    将add_with_tax()函数的声明与定义分离

--- totaller.c
+++ totaller.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
+#include "totaller.h"
 
 float total = 0.0;
 short count = 0;
 short tax_percent = 6;
-
-float add_with_tax(float f)
-{
-    float tax_rate = 1 + tax_percent / 100.0;
-    total = total + (f * tax_rate);
-    count = count + 1;
-
-    return total;
-}
 
 int main()
 {
@@ -25,4 +17,13 @@ int main()
     printf("Number of items: %hi\n", count);
 
     return 0;
+}
+
+float add_with_tax(float f)
+{
+    float tax_rate = 1 + tax_percent / 100.0;
+    total = total + (f * tax_rate);
+    count = count + 1;
+
+    return total;
 }
