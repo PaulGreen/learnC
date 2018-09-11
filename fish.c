0db2cb1eb75c5a70c8969a59c414d823f762fdc9
Bo Vh <zhangbo9299@gmail.com>
2018-09-11 22:26:42 +0800

    add function label()

--- fish.c
+++ fish.c
@@ -28,13 +28,19 @@ void catalog(struct fish f)
     printf("%s is a %s with %i teeth. He is %i\n", f.name, f.species, f.teeth, f.age);
 }
 
+void label(struct fish a)
+{
+    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n", a.name, a.species, a.age, a.teeth);
+    printf("Feed with %2.2f lbs of %s and allow to %s for %2.2f hours\n", a.care.food.weight, a.care.food.ingredients, a.care.exercise.description, a.care.exercise.duration);
+}
+
 int main()
 {
 
     struct fish snappy = {"Snappy", "Piranha", 69, 4, {{"Meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
 
     catalog(snappy);
-    printf("Snappy 喜欢吃 %s", snappy.care.food);
-    printf("Snappy 喜欢锻炼 %f hours", snappy.care.exercise_hours);
+    label(snappy);
+    
     return 0;
 }
